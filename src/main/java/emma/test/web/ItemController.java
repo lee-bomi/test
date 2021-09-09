d2ee53f713786690d8d664ea6653cfbaf41686df
lee-bomi <bomvll@naver.com>
2021-09-09 17:59:04 +0900

    add html(item.html)

--- src/main/java/emma/test/web/ItemController.java
+++ src/main/java/emma/test/web/ItemController.java
@@ -3,6 +3,7 @@ package emma.test.web;
 import emma.test.domain.Item;
 import emma.test.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/basic/items")
 @Controller
@@ -39,7 +41,7 @@ public class ItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("add") //새로고침했을때 최근 작업이 반복지않도록, 리다이렉트
+    @PostMapping("/add") //새로고침했을때 최근 작업이 반복지않도록, 리다이렉트
     public String addItem(Item item, RedirectAttributes redirectAttributes) {
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -47,10 +49,17 @@ public class ItemController {
         return "redirect:/basic/items/{itemId}";
     }
 
-    @GetMapping("{itemId}")
+    @GetMapping("/{itemId}")
     public String item(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editform";
     }
 }
